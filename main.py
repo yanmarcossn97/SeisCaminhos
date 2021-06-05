ae470c702e64a4bde6c487efc2a5ee0c910e146a
Yan Marcos <yanmsn97@gmail.com>
2021-06-05 12:03:06 -0300

    gitignore

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import discord
+import requests
+import json
+import translateText
 
-TOKEN = # my bot token
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+TOKEN = # token of the bot
+channel_id = # channel id
 
 client = discord.Client()
+
+def get_quote(arg):
+	response = requests.get(arg)
+	json_data = json.loads(response.text)
+	quote = json_data[0]['q'] + " _" + json_data[0]['a']
+
+	return quote
+
+async def daily_quote():
+  channel = client.get_channel(channel_id)
+  quote_en = get_quote("https://zenquotes.io/api/today")
+  quote_pt = translateText.translate_text(quote_en)
+  await channel.send(quote_pt)
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
+  # initializig scheduler
+  scheduler = AsyncIOScheduler()
+
+  # scheduling a daily message function
+  scheduler.add_job(daily_quote, 'cron', hour=8)
+
+  # starting scheduler
+  scheduler.start()
+
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
-     
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello Gamers!')
+
+  if message.content.startswith('$insp'):
+    quote_en = get_quote("https://zenquotes.io/api/random")
+    quote_pt = translateText.translate_text(quote_en)
+    await message.channel.send(quote_pt)
 
 client.run(TOKEN)
--- translateText.py
+++ translateText.py
@@ -1,15 +1,19 @@
 from os import environ
-
 from google.cloud import translate
 
-project_id = environ.get("PROJECT_ID", "")
-assert project_id
-parent = f"projects/{project_id}"
-client = translate.TranslationServiceClient()
+def translate_text(text):
+	project_id = environ.get("PROJECT_ID", "")
+	assert project_id
+	parent = f"projects/{project_id}"
+	client = translate.TranslationServiceClient()
 
-response = client.get_supported_languages(parent=parent, display_language_code="en")
-languages = response.languages
+	target_language_code = "pt"
 
-print(f" Languages: {len(languages)} ".center(60, "-"))
-for language in languages:
-    print(f"{language.language_code}\t{language.display_name}")
+	response = client.translate_text(
+	    contents=[text],
+	    target_language_code=target_language_code,
+	    parent=parent,
+	)
+
+	for translation in response.translations:
+	    return(translation.translated_text)
